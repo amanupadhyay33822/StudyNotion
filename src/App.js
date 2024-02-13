7e9dea7b9a2084c546f7883d5d72438783702d76
amanupadhyay33822 <amanupadhyay33822@gmail.com>
2024-02-13 09:41:31 +0530

    jhe====

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import Homepage from "./pages/Homepage";
+import Test from "./pages/Test";
 
 function App() {
   return (
     <div className="w-screen min-h-screen flex flex-col font-inter bg-richblack-900">
       <Routes>
         <Route path="/" element={<Homepage/>}></Route>
+        <Route path="/test" element={<Test/>}></Route>
       </Routes>
     </div>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import Button from "../components/core/HomePage/Button"
+import Button from "../components/core/HomePage/Button";
 import HighlightText from "../components/core/HomePage/HighlightText";
-import Banner from "../assets/Images/banner.mp4"
-import CodeBlock from "../components/core/HomePage/CodeBlocks"
+import Banner from "../assets/Images/banner.mp4";
+import CodeBlock from "../components/core/HomePage/CodeBlocks";
 const Homepage = () => {
   return (
     <div>
@@ -40,59 +40,123 @@ const Homepage = () => {
           including hands-on projects, quizzes, and personalized feedback from
           instructors.
         </div>
-         {/* for 2 button */}
-         <div className="flex gap-7 mt-5">
-         <Button active={true} linkto="/signup">Learn More</Button>
-       <Button active={false} linkto="/login">Book a Demo</Button>
-         </div>
-       
-          <div className="shadow-blue-200 mx-3 my-14">
-            <video
-             muted
-             autoPlay
-             loop
-            >
-             <source src={Banner}></source>
-            </video>
-          </div>
-           {/* Code Section 1 */}
-          <div>
-          <CodeBlock 
-                position={"lg:flex-row"}
-                // done by aman randi ka
-                heading={
-                    <div className='text-4xl font-semibold'>
-                        Unlock Your
-                        <HighlightText text={"coding potential"}/>
-                        with our online courses
-                    </div>
-                }
-                subheading = {
-                    "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
-                }
-                ctabtn1={
-                    {
-                        btnText: "try it yourself",
-                        linkto: "/signup",
-                        active: true,
-                    }
-                }
-                ctabtn2={
-                    {
-                        btnText: "learn more",
-                        linkto: "/login",
-                        active: false,
-                    }
-                }
-
-                codeblock={`<<!DOCTYPE html>\n<html>\nhead><title>Example</title><linkrel="stylesheet"href="styles.css">\n/head>\n`}
-                codeColor={"text-yellow-25"}
-            />
+        {/* for 2 button */}
+        <div className="flex gap-7 mt-5">
+          <Button active={true} linkto="/signup">
+            Learn More
+          </Button>
+          <Button active={false} linkto="/login">
+            Book a Demo
+          </Button>
         </div>
-         {/* Code Section 2 */}
+
+        <div className="shadow-blue-200 mx-3 my-14">
+          <video muted autoPlay loop>
+            <source src={Banner}></source>
+          </video>
+        </div>
+        {/* Code Section 1 */}
+        <div>
+          <CodeBlock
+            position={"lg:flex-row"}
+            // done by aman
+            heading={
+              <div className="text-4xl font-semibold">
+                Unlock Your
+                <HighlightText text={"coding potential"} />
+                with our online courses
+              </div>
+            }
+            subheading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "try it yourself",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "learn more",
+              linkto: "/login",
+              active: false,
+            }}
+            codeblock={`<<!DOCTYPE html>\n<html>\nhead><title>Example</title><linkrel="stylesheet"href="styles.css">\n/head>\n`}
+            codeColor={"text-yellow-25"}
+          />
+        </div>
+        {/* Code Section 2 */}
+        <div>
+          <CodeBlock
+            position={"lg:flex-row-reverse"}
+            heading={
+              <div className="text-4xl font-semibold">
+                Unlock Your
+                <HighlightText text={"coding potential"} />
+                with our online courses
+              </div>
+            }
+            subheading={
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+            }
+            ctabtn1={{
+              btnText: "try it yourself",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "learn more",
+              linkto: "/login",
+              active: false,
+            }}
+            codeblock={`<<!DOCTYPE html>\n<html>\nhead><title>Example</title><linkrel="stylesheet"href="styles.css">\n/head>\n`}
+            codeColor={"text-yellow-25"}
+          />
+        </div>
       </div>
 
       {/* section-2 (white) */}
+
+      <div className="bg-pure-greys-5 text-richblack-700">
+        <div className="homepage_bg h-[310px]">
+          <div className="w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto">
+            <div className="h-[150px]"></div>
+            <div className="flex flex-row gap-7 text-white ">
+              <Button active={true} linkto={"/signup"}>
+                <div className="flex items-center gap-3">
+                  Explore Full Catalog
+                  <FaArrowRight />
+                </div>
+              </Button>
+              <Button active={false} linkto={"/signup"}>
+                <div>Learn more</div>
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-11/12 bg-white  mx-auto max-w-maxContent flex flex-col items-center justify-between">
+          
+        <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+                    <div className='text-4xl font-semibold w-[45%]'>
+                        Get the Skills you need for a
+                        <HighlightText text={"Job that is in demand"} />
+                    </div>
+
+                    <div className='flex flex-col gap-10 w-[40%] items-start'>
+                    <div className='text-[16px]'>
+                    The modern StudyNotion is the dictates its own terms. Today, to be a competitive specialist requires more than professional skills.
+                    </div>
+                    <Button active={true} linkto={"/signup"}>
+                        <div>
+                            Learn more
+                        </div>
+                    </Button>
+                    </div>
+
+                </div>
+        </div>
+      </div>
+
       {/* section-3 (blue) */}
       {/* section-4 (footer) */}
     </div>
